39fd402dd9e91bb3b529fabf3e53a3d476b1a962
nuwandavek <vivekaithal44@gmail.com>
2021-01-03 16:19:25 -0800

    working extension and flask app

--- you-ext/content_scripts/you.js
+++ you-ext/content_scripts/you.js
@@ -3,10 +3,10 @@ document.body.style.border = '5px solid red';
 console.log("Hello");
 
 $.ajax({
-  url: 'http://localhost:5000',
+  url: 'http://localhost:5000/autocomplete',
   crossDomain: true,
   dataType: 'json',
-  // // data: data,
+  data: {context : 'Vivek : Hi!'},
   success: (d)=>{
     console.log(d)
   }
