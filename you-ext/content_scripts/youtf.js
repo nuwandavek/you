63de7b5c3983f8a60c7089f0fe0124cafcdbab06
nuwandavek <vivekaithal44@gmail.com>
2021-01-04 19:44:48 -0800

    add working server, extension

--- you-ext/content_scripts/youtf.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    console.log('hellohello');
-    browser.runtime.onMessage.addListener((message) => {
-        console.log('yo')
-        console.log(message);
-    });
-  
-  })();
-  
